588f6d0d7b9cfd270f1bff0c43db15de7aa5fbc3
Ilia Popov <ilia_popov02@abv.bg>
2023-05-26 13:36:49 +0300

    Remove validations from constructor

--- src/models/toothpaste.js
+++ src/models/toothpaste.js
@@ -14,7 +14,15 @@ export class Toothpaste extends Product {
   */
   constructor(name, brand, price, gender, ingredients) {
     super(name, brand, price, gender);
+    this.#ingredients = ingredients;
+  }
 
+  /**
+   * Validation for ingredients.
+   * @param {string} ingredients Ingredients to check.
+   */
+
+  ingredientsValidation(ingredients) {
     if (!ingredients) {
       throw new Error('Cannot pass invalid value!');
     }
@@ -22,10 +30,8 @@ export class Toothpaste extends Product {
     if (typeof ingredients !== 'string') {
       throw new Error('Ingredients must be string!');
     }
-
-    this.#ingredients = ingredients;
   }
-
+  
   get ingredients() {
     return this.#ingredients;
   }
